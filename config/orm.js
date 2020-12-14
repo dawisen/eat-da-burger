3b1b8101bf425f7b2fdeb3ad059c8be01ccf710b
gudetama <daniellewwise@gmail.com>
2020-12-14 16:46:38 -0500

    added missing helper functions

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,31 @@
 var connection = require('./connection');
 
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
 const orm = {
     selectAll: function (tableInput, serverFunc) {
         var queryString = "SELECT * FROM " + tableInput + ";";
